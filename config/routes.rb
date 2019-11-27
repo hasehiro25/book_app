122fc3ee528b72256771d2f99ea618fbeeb40813
hasehiro25 <hase1969.hiro@gmail.com>
2019-11-27 20:30:04 +0900

    Change to not require password for edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "users/registrations"}
   root "books#index"
   resources :books
 end
