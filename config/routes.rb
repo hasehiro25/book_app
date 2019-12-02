b07d299619b8abb5e7b753a7de6fa61d95333964
hasehiro25 <hase1969.hiro@gmail.com>
2019-12-02 14:13:06 +0900

    Fix rubocop offenses

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: "users/registrations"}
+  devise_for :users, controllers: { registrations: "users/registrations" }
   resources :users,  only: [:show, :index]
   root "books#index"
   resources :books
