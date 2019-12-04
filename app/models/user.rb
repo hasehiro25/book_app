650b4aa6c706bf868e3d0c6a8cb104c17431ff49
hasehiro25 <hase1969.hiro@gmail.com>
2019-12-04 13:33:35 +0900

    Add to signin with github

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable
 
   VALID_POSTCODE = /\A\z|\A\d{7}\z/
   validates :postcode, format: { with: VALID_POSTCODE }
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
 end
