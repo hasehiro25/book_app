e0471951b8bc3f7e82036975a4cc15ca23b0c4b4
hasehiro25 <hase1969.hiro@gmail.com>
2019-12-04 21:39:26 +0900

    Add unique to [uid provider]

--- db/migrate/20191203084601_add_omni_auth_to_users.rb
+++ db/migrate/20191203084601_add_omni_auth_to_users.rb
@@ -3,6 +3,7 @@
 class AddOmniAuthToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :provider, :string
-    add_column :users, :uid, :string, index: true
+    add_column :users, :uid, :string
   end
+  add_index :users, [:uid, :provider], unique: true
 end
