233f3a7f7b8d12a05a3b90225bb8bf1bfada689c
hasehiro25 <hase1969.hiro@gmail.com>
2019-12-06 22:43:47 +0900

    Fix bug to accept edit without check

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,8 @@ class User < ApplicationRecord
 
   VALID_POSTCODE = /\A\z|\A\d{7}\z/
   validates :postcode, format: { with: VALID_POSTCODE }
-  validates :delete_avatar_check, acceptance: true
 
+  attr_accessor :delete_avatar_check
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
