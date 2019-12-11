0be7185fda51cf58e468d47c3e47608bda5002fb
hasehiro25 <hase1969.hiro@gmail.com>
2019-12-11 16:17:29 +0900

    Add foreign_key to follower and follwed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_09_162030) do
+ActiveRecord::Schema.define(version: 2019_12_11_070310) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -76,4 +76,6 @@ ActiveRecord::Schema.define(version: 2019_12_09_162030) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "books", "users"
+  add_foreign_key "relationships", "users", column: "followed_id"
+  add_foreign_key "relationships", "users", column: "follower_id"
 end
