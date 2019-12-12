acece0ec637d68c28b8711cae7765eea8e340254
hasehiro25 <hase1969.hiro@gmail.com>
2019-12-12 17:15:05 +0900

    Add japanese localize

--- app/controllers/user/following_controller.rb
+++ app/controllers/user/following_controller.rb
@@ -9,13 +9,13 @@ class User::FollowingController < ApplicationController
 
   def create
     current_user.followings.create!(followed: User.find(params[:user_id]))
-    redirect_to user_following_url(current_user), notice: "フォローしました"
+    redirect_to user_following_url(current_user), notice: t("followings.create.success")
   rescue ActiveRecord::RecordNotUnique
-    redirect_to user_following_url(current_user), alert: "既にフォローしています"
+    redirect_to user_following_url(current_user), alert: t("followings.create.already_followed")
   end
 
   def destroy
     current_user.followings.find_by(followed: User.find(params[:user_id])).destroy
-    redirect_to user_following_url(current_user), notice: "フォローから外しました"
+    redirect_to user_following_url(current_user), notice: t("followings.destroy.success")
   end
 end
