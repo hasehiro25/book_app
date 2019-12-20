a0bac5f292c27d1c28007b53b8155ff5d4d6adc3
hasehiro25 <hase1969.hiro@gmail.com>
2019-12-20 21:12:28 +0900

    Add method deleted from book model

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -60,7 +60,11 @@ class BooksController < ApplicationController
 
     def verify_posted_user
       # 403ページ未作成
-      head :forbidden unless @book.same_user?(current_user)
+      head :forbidden unless same_user?(@book.user)
+    end
+
+    def same_user?(user)
+      user == current_user
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
